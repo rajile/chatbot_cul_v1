b0ad938ba15698f66d99da2bbbd8cb410a1dd369
rajile <rajile601@gmail.com>
2026-03-03 20:06:58 -0500

    Add files via upload

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,13 +5,13 @@ import os
 
 # La URL de TiDB se obtendrá de variables de entorno en producción (Railway)
 # En local, se usarán por defecto tus credenciales de TiDB Cloud
-TIDB_URL = "mysql+mysqlconnector://2czmxYCNTp9AbwJ.root:mR8V2hc3yVyRKyny@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/cul_chatbot"
+TIDB_URL = "mysql+pymysql://2czmxYCNTp9AbwJ.root:mR8V2hc3yVyRKyny@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/cul_chatbot"
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", TIDB_URL)
 
 # Para TiDB Cloud Serverless necesitamos conectarnos por SSL
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"ssl_verify_identity": True, "ssl_verify_cert": True}
+    connect_args={"ssl": {"ca": "/etc/ssl/certs/ca-certificates.crt"}}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
